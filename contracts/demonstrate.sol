43c7eafe743f39a23ab7087d351cee14375f198b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-02-21 11:44:34 +1000

    Update

--- contracts/demonstrate.sol
+++ contracts/demonstrate.sol
@@ -1,36 +1,26 @@
 pragma solidity ^0.6.0;
 
 contract Demonstrate {
-
-    address public owner;
-    uint256 public fee;
-
+    
     struct Demonstration {
         uint256 startTime;
         bytes32[] whatThreeWords;
         uint256 dontations;
         address owner;
-        bytes32 proofOfAttenance;
     }
 
     Demonstration[] public demonstrations;
-    mapping(address => uint256) public claims;
+    //mapping(address => uint256) public claims;
 
-    constructor() public {
-        owner = msg.sender;
-        fee = 0;
-    }
-
-    function add(uint256 startTime, bytes32 location1, bytes32 location2, bytes32 location3, bytes32 poa) public payable returns (uint) {
-        require(msg.value >= fee, "Fee too low.");
-        require(startTime > now, "Campaigns need to be in the future.");
+    function add(uint256 startTime, bytes32 location1, bytes32 location2, bytes32 location3) public payable returns (uint) {
+        require(startTime > now, "Demonstrations need to be in the future.");
 
         bytes32[] memory location = new bytes32[](3);
         location[0] = location1;
         location[1] = location2;
         location[2] = location3;
 
-        demonstrations.push(Demonstration(startTime, location, msg.value - fee, msg.sender));
+        demonstrations.push(Demonstration(startTime, location, msg.value, msg.sender));
         emit NewDemonstration(demonstrations.length - 1);
 
         return demonstrations.length - 1;
@@ -43,16 +33,11 @@ contract Demonstrate {
         emit DonationReceived(_index, msg.value);
     }
 
-    function claim(uint256 _index, bytes32 poa) public {
-        require(demonstrations[_index].startTime > now, "Must claim after event");
+    function payout(uint256 _index) public onlyCampaigOnwner(_index) {
 
     }
 
-    function payout(uint256 _index) public {
-
-    }
-
-    modifier onlyCampaignOwner(uint256 _index) {
+    modifier onlyCampaigOnwner(uint256 _index) {
         require(demonstrations[_index].owner == msg.sender, "Only owner");
         _;
     }
--- test/demonstratetests.js
+++ test/demonstratetests.js
@@ -1,7 +1,7 @@
 const Demonstrate = artifacts.require("Demonstrate");
 const helper = require("./helpers/truffleTestHelper");
 
-const DEC_25 = 1577275200;
+const DEC_25_2020 = 1608897600;
 
 //cover rails metro
 const WHAT_3_WORDS = ["0x636f766572", "0x7261696c73", "0x6d6574726f"];
@@ -19,10 +19,9 @@ contract.only("Demonstrate", function(accounts) {
 
   describe("Campaign tests", () => {
     it("should add new campaign", async function () {
-      const actual = await contractInstance.add(DEC_25, WHAT_3_WORDS[0], WHAT_3_WORDS[1], WHAT_3_WORDS[2]);
+      const actual = await contractInstance.add(DEC_25_2020, WHAT_3_WORDS[0], WHAT_3_WORDS[1], WHAT_3_WORDS[2]);
       
-      console.log(actual);
-      //assert.equal(Number(actual), 0, "Should be index 0");
+      
     });
   });
 });
