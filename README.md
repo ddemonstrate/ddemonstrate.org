20b69893d31c12caf6e4b4ff0283694a3a10cbb1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-03-04 14:25:28 +1000

    Add local node

--- README.md
+++ README.md
@@ -63,6 +63,7 @@ Listing a campaign is made simple by entering the title, purpose and category (e
 
 Code will be commited under the `/contracts` directory.  Testnet key `tragic near rocket across biology shop push dragon jazz detail differ say`
 
+Rinkby: 
 Ropsten: `0x24b6e8aa5adca2bb30fc3c28159b96755d6930e6`
 
 ## Thanks
--- add_event.py
+++ add_event.py
@@ -1,15 +1,18 @@
 from web3 import Web3
 import json
 
-infura_url = "https://ropsten.infura.io/v3/eaf5e0b4a01042a48211762c8d4eec44"
-web3 = Web3(Web3.HTTPProvider(infura_url))
+#node_url = "https://ropsten.infura.io/v3/eaf5e0b4a01042a48211762c8d4eec44"
+node_url = "http://192.168.1.104:8545" #LAN GETH Mainnet
+web3 = Web3(Web3.HTTPProvider(node_url))
 
 print(web3.isConnected())
 print(web3.eth.blockNumber)
 
 owner = "0xa1138fccd5f8E126E8d779CF78a547517307559d"
 address = "0x23da3e3Cd9400B624169b76A87325e4E4F7bC0E0"
-balance = web3.eth.getBalance(address)
+balance = web3.eth.getBalance(owner)
+
+balance = web3.eth.getBalance("ddemonstrate.eth")
 
 with open("build/contracts/Demonstrate.json", "r") as f:
     _json = json.load(f)
